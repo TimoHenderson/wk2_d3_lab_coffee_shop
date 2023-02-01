041727021f07e95a9240e1a0e4cd5723df2323bb
TimoHenderson <tjhenderson2@gmail.com>
2023-02-01 14:18:13 +0000

    Added drinks to CoffeeShop

--- tests/coffee_shop_test.py
+++ tests/coffee_shop_test.py
@@ -1,10 +1,17 @@
 import unittest
 from src.coffee_shop import CoffeeShop
+from src.drink import Drink
 
 
 class TestCoffeeShop(unittest.TestCase):
     def setUp(self):
-        self.coffee_shop = CoffeeShop("The Prancing Pony", 100.00)
+        self.drinks = [
+            Drink("Mocha", 5.00),
+            Drink("Latte", 4.50),
+            Drink("Hot Chocolate", 3.60),
+            Drink("Tea", 2.00),
+        ]
+        self.coffee_shop = CoffeeShop("The Prancing Pony", 100.00, self.drinks)
 
     def test_has_name(self):
         self.assertEqual("The Prancing Pony", self.coffee_shop.name)
@@ -15,3 +22,6 @@ class TestCoffeeShop(unittest.TestCase):
     def test_increase_till(self):
         self.coffee_shop.increase_till(10.00)
         self.assertEqual(110.00, self.coffee_shop.till)
+
+    def test_has_drinks(self):
+        self.assertEqual(self.drinks, self.coffee_shop.drinks)
--- tests/drink_test.py
+++ tests/drink_test.py
@@ -8,3 +8,6 @@ class TestDrink(unittest.TestCase):
 
     def test_has_name(self):
         self.assertEqual("Mocha", self.drink.name)
+
+    def test_has_price(self):
+        self.assertEqual(5.00, self.drink.price)
